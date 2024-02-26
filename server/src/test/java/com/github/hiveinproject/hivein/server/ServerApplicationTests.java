e9d371e64b569e83462baa210da8623eb3458f9c
Marcelo Fidelis <marcelofiddelis201@gmail.com>
2024-02-26 13:03:02 -0300

    feat: Added server project

---
+++ server/src/test/java/com/github/hiveinproject/hivein/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hiveinproject.hivein.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
